245f2796542330b225ecfbd7f76187369d31a6f7
suxiaolei <yinsxi@163.com>
2017-11-18 10:27:16 +0800

    updata

--- app/src/main/java/com/reosrose/utildemo/activity/BaseActivity.java
+++ app/src/main/java/com/reosrose/utildemo/activity/BaseActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 import com.reosrose.utildemo.R;
+import com.reosrose.utildemo.utils.ActivityCollector;
 
 public abstract class BaseActivity extends AppCompatActivity {
 
@@ -16,8 +17,12 @@ public abstract class BaseActivity extends AppCompatActivity {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         //横屏
 //        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-
+        ActivityCollector.addActivity(this);
     }
 
-
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ActivityCollector.removeActivity(this);
+    }
 }
